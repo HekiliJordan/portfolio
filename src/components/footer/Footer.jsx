3a8c483bb439c432c747e27811b1a53470cd72b7
hekili <hekili.jordan@wsu.edu>
2023-06-04 19:06:51 -0700

    Adding in pages and photos

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Image from "next/image";
+
+const Footer = () => {
+    return (
+        <div>
+            <div >
+                <div>©2023 Key-Web . All rights reserved.</div>
+                <div >
+                    <Image src="/1.png" width={15} height={15} alt="Key-Web Facebook Account" />
+                    <Image src="/2.png" width={15} height={15} alt="Key-Web " />
+                    <Image src="/3.png" width={15} height={15} alt="Key-Web " />
+                    <Image src="/4.png" width={15} height={15} alt="Key-Web " />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
