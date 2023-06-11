9cbaf57ae5da047c39a968bf7ab9d184386c7342
hekili <hekili.jordan@wsu.edu>
2023-06-11 15:36:06 -0700

    Adding backend

---
+++ src/models/Post.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const {Schema} = mongoose
+
+const poseSchema = new Schema({
+        name: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        }
+    },
+    {timestamps: true}
+)
